65f7cd950c107bb941a8a4cdc55b18668467edad
joao <joao.ol.dev@gmail.com>
2025-05-27 15:26:54 -0300

    inicio do projeto GuessHunter com lavarel

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
+            $table->interger('gamesplayed') ->default = 0;
             $table->timestamps();
         });
 
